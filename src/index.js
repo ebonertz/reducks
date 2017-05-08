4377ffd9bb67ae1581aa7b88b33b24f46f691c9f
Evan Bonertz <evan.bonertz@commercetools.com>
2017-05-08 17:46:06 -0400

    Initial Committ

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+//Create a React component
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+//Take the components generated html and put it in the DOM 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
